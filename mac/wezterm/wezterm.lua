53b8d4e9b2e3a37ffa113b6fa46b59e35e19bb1c
Nyi Nyi <nyinyi.than@ahrefs.com>
2023-10-04 04:49:15 +0800

    updated

---
+++ mac/wezterm/wezterm.lua
@@ -0,0 +1,26 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This table will hold the configuration.
+local config = {
+    font = wezterm.font("Monolisa Variable"),
+    font_size = 12,
+}
+-- config.font = wezterm.font_with_fallback {
+--     'Monolisa Variable',
+--     'FuraMono Nerd Font Mono',
+-- }
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+    config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = 'Gruvbox Dark (Gogh)'
+
+-- and finally, return the configuration to wezterm
+return config
